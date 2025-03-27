b4a6b42ab51c228a38de815f6bb306a1f59c72d0
Sugar500 <j.gramer@digipen.edu>
2025-03-27 12:40:26 -0700

    Refactor `initProjectPage` and improve event handling.
    Moved `initProjectPage` to `subpage-init.js` for better modularity and updated its invocation via a custom event in `posts-functions.js`. Removed duplicate code and streamlined initialization logic for project pages.

--- scripts/subpage-init.js
+++ scripts/subpage-init.js
@@ -96,11 +96,84 @@ function initDirectory(tables, posts) {
         "creative", "A landing place for projects"
     ]);
 
-    posts.posts.forEach(function (post, index) {
+    posts.posts.forEach(function (post) {
         tables.addRow(table, [
             "<a href='./article-page.html#" + post["Title"].toLowerCase().
             replaceAll(' ', '-') + "'>" + post["Title"] + "</a>",
             post["Tags"].join(", "),
+            post["Short Summary"]
+        ])
+    })
+}
+
+function initProjectPage(hash, tables, posts) {
+    // update the banner to match the projects
+    const bannerTitle = document.querySelector('.banner-small h2');
+    const bannerSubtitle = document.querySelector('.banner-small p');
+    switch (hash) {
+        case "blog":
+            bannerTitle.title = "";
+            bannerTitle.innerHTML = "Welcome to the Blog Archive!";
+            bannerSubtitle.title = "the non-secret ones";
+            bannerSubtitle.innerHTML = "A place to find all of the blog posts.";
+            break;
+        case "projects":
+            bannerTitle.title = "Are there any completed ones?";
+            bannerTitle.innerHTML = "Welcome to the Project Archive!";
+            bannerSubtitle.title = "";
+            bannerSubtitle.innerHTML = "A place to find all the past and current projects.";
+            break;
+        case "creative-projects":
+            bannerTitle.title = "";
+            bannerTitle.innerHTML = "Welcome to the creative project archive!";
+            bannerSubtitle.title = "Most of the should direct to more archives! Homepage \'ception.";
+            bannerSubtitle.innerHTML = "A place to find the creative projects.";
+            break;
+        default:
+            bannerTitle.title = "Ya probably shouldn't be here.";
+            bannerTitle.innerHTML = "Welcome to the Archive!";
+            bannerSubtitle.title = "";
+            bannerSubtitle.innerHTML = "A place to find all of the posts.";
+    }
+
+    // update the featured articles
+    const featuredPosts = document.querySelector('[class*="featured-posts-"]');
+    const featuredPostsHeading = document.querySelector('[class*="featured-posts-"] h2');
+    featuredPosts.classList.remove('blog-posts');
+    featuredPosts.classList.remove('project-posts');
+    featuredPosts.classList.remove('creative-posts');
+    switch (hash) {
+        case "blog":
+            featuredPosts.classList.add('blog-posts');
+            featuredPostsHeading.innerHTML = "Featured Blog Posts";
+            break;
+        case "projects":
+            featuredPosts.classList.add('project-posts');
+            featuredPostsHeading.innerHTML = "Featured Projects";
+            break;
+        case "creative-projects":
+            featuredPosts.classList.add('creative-posts');
+            featuredPostsHeading.innerHTML = "Featured Project Posts!";
+            break;
+        default:
+            featuredPostsHeading.innerHTML = "Featured Posts";
+            break;
+    }
+
+    // grab all posts matching tags
+    const postTag = hash === "projects" ? "project" : hash === "creative-projects" ? "creative"
+        : hash === "blog" ? "blog" : "posts";
+    const filtered = posts.filterTag(postTag);
+
+    // update the table
+    const table = Array.from(document.getElementsByTagName('table'))[0];
+    tables.clearHeader('table');
+    tables.addHeader(table, ["Title", "Description"]);
+    tables.clearBody('table');
+    filtered.forEach(function (post) {
+        tables.addRow(table, [
+            "<a href='./article-page.html#" + post["Title"].toLowerCase().
+            replaceAll(' ', '-') + "'>" + post["Title"] + "</a>",
             post["Short Summary"]
         ])
     })
