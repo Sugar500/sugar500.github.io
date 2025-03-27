82bf067dacdcd6d0e984effccf4eb6bfd931c1af
Sugar500 <j.gramer@digipen.edu>
2025-03-27 12:21:34 -0700

    Refactor event listeners and improve table manipulation.
    Wrapped event listeners in DOMContentLoaded for proper initialization. Added checks for table elements in manipulation methods to prevent errors on missing sections. Streamlined initialization process by introducing the 'initialize-clear' event.

--- scripts/listeners.js
+++ scripts/listeners.js
@@ -1,102 +1,108 @@
 import {initPosts, Posts} from "./posts-functions.js";
 
-document.addEventListener('loaded-component', function(event) {
-    switch (event.detail.page) {
-        case "menu":
-            initMenu();
-            break;
-        case "header":
-            initHeader();
-            break;
-        case "banner":
-            initBanner();
-            break;
-        case "footer":
-            initFooter();
-            break;
-    }
-})
-
-document.addEventListener('loaded-posts', function (event) {
-    event.detail.posts.forEach((post) => {
-        Posts.add(post);
+document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener('loaded-component', function(event) {
+        switch (event.detail.page) {
+            case "menu":
+                initMenu();
+                break;
+            case "header":
+                initHeader();
+                break;
+            case "banner":
+                initBanner();
+                break;
+            case "footer":
+                initFooter();
+                break;
+        }
     })
 
-    if (!Posts.loaded) return;
-    initPosts(new URL(document.location));
-})
+    document.addEventListener('loaded-posts', function (event) {
+        event.detail.posts.forEach((post) => {
+            Posts.add(post);
+        })
 
-document.addEventListener('loaded-settings', function () {
-    Posts.loaded = true;
-
-    if (Posts.posts.length < 1) return;
-    initPosts(new URL(document.location));
-})
-
-window.addEventListener('hashchange', (event) => {
-    initPosts(new URL(event.newURL));
-})
-
-document.addEventListener('import-component', function (event) {
-    const elements = document.querySelectorAll("." + event.detail.target);
-    elements.forEach(element => {
-        element.outerHTML = event.detail.data;
-        document.dispatchEvent(new CustomEvent('loaded-component', {
-            bubbles: true,
-            detail: {
-                page: event.detail.target
-            }
-        }))
+        if (!Posts.loaded) return;
+        initPosts(new URL(document.location));
     })
-})
 
-document.addEventListener('import-settings', function (event) {
-    if (!event.detail.data.hasOwnProperty("Replacements")) {
-        console.warn("Replacements was not found in settings");
-        return;
-    }
+    document.addEventListener('loaded-settings', function () {
+        Posts.loaded = true;
 
-    event.detail.data["Replacements"].forEach(element => {
-        // noinspection JSUnresolvedReference
-        const target = document.querySelector(element.Structure);
-        if (target === null) {
-            // noinspection JSUnresolvedReference
-            console.error(`There was an error finding ${element.Structure}`);
+        if (Posts.posts.length < 1) return;
+        initPosts(new URL(document.location));
+    })
+
+    window.addEventListener('hashchange', (event) => {
+        initPosts(new URL(event.newURL));
+    })
+
+    document.addEventListener('import-component', function (event) {
+        const elements = document.querySelectorAll("." + event.detail.target);
+        elements.forEach(element => {
+            if (element === null) return;
+            element.outerHTML = event.detail.data;
+            document.dispatchEvent(new CustomEvent('loaded-component', {
+                bubbles: true,
+                detail: {
+                    page: event.detail.target
+                }
+            }))
+        })
+    })
+
+    document.addEventListener('import-settings', function (event) {
+        if (!event.detail.data.hasOwnProperty("Replacements")) {
+            console.warn("Replacements was not found in settings");
             return;
         }
 
-        if (element.hasOwnProperty('Class')) {
-            target.classList.add(element.Class);
-        }
-
-        if (element.hasOwnProperty('Title')) { // noinspection JSUnresolvedReference
-            target.title = element.Title;
-        }
-
-        if (element.hasOwnProperty('Content')) {
-            target.innerHTML = "";
+        event.detail.data["Replacements"].forEach(element => {
             // noinspection JSUnresolvedReference
-            element.Content.forEach((child) => {
-                target.innerHTML += child;
-            });
-        }
+            const target = document.querySelector(element.Structure);
+            if (target === null) {
+                // noinspection JSUnresolvedReference
+                console.error(`There was an error finding ${element.Structure}`);
+                return;
+            }
 
-        if (element.hasOwnProperty('Replace-Classes')) {
-            element['Replace-Classes'].forEach(replaceClass => {
-                target.classList.replace(replaceClass["Current-Class"], replaceClass["New-Class"]);
-            })
+            if (element.hasOwnProperty('Class')) {
+                target.classList.add(element.Class);
+            }
+
+            if (element.hasOwnProperty('Title')) { // noinspection JSUnresolvedReference
+                target.title = element.Title;
+            }
+
+            if (element.hasOwnProperty('Content')) {
+                target.innerHTML = "";
+                // noinspection JSUnresolvedReference
+                element.Content.forEach((child) => {
+                    target.innerHTML += child;
+                });
+            }
+
+            if (element.hasOwnProperty('Replace-Classes')) {
+                element['Replace-Classes'].forEach(replaceClass => {
+                    target.classList.replace(replaceClass["Current-Class"], replaceClass["New-Class"]);
+                })
+            }
+        })
+    })
+
+    document.addEventListener('initialized-posts', function (event) {
+        switch (event.detail.page) {
+            case "directory":
+                initDirectory(event.detail.data[0], event.detail.data[1]);
+                break;
+            case "article":
+                break;
+            case "project":
+                break;
         }
     })
-})
 
-document.addEventListener('initialized-posts', function (event) {
-    switch (event.detail.page) {
-        case "directory":
-            initDirectory(event.detail.data[0], event.detail.data[1]);
-            break;
-        case "article":
-            break;
-        case "project":
-            break;
-    }
-})
+    document.dispatchEvent(new CustomEvent('initialize-clear'));
+});
+
