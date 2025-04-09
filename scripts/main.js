1b2288fd02f02d1023429e4e77e12d88cf8ea32f
Sugar500 <j.gramer@digipen.edu>
2025-04-09 14:44:42 -0700

    Update sort order and add new blog and project data
    Reversed sort order by LastModified in post functions for correct chronological display. Added a new blog (Apr-9-2025) and project (Brewl) data files along with related assets, including a game trailer video for Brewl. Updated main.js to load these new items.

--- scripts/main.js
+++ scripts/main.js
@@ -69,6 +69,8 @@ function loadSettings(fileSource) {
 
 function loadPosts(fileSource) {
     const postFiles = [
+        "blogs/Apr-9-2025",
+        "projects/brewl",
         "notes/User-Stories-in-Game-Development",
         "blogs/Apr-2-2025",
         "projects/witchs-brew",
